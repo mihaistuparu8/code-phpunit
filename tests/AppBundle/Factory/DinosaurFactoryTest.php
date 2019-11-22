cffa94e42b5ec7535883c6eb743ae2d2eccc5941
Mihai Stuparu <mihai.stuparu@zitec.com>
2019-11-22 23:41:19 +0200

    Wip

---
+++ tests/AppBundle/Factory/DinosaurFactoryTest.php
@@ -0,0 +1,74 @@
+<?php
+
+
+namespace Tests\AppBundle\Factory;
+
+
+use AppBundle\Entity\Dinosaur;
+use PHPUnit\Framework\TestCase;
+use AppBundle\Factory\DinosaurFactory;
+
+class DinosaurFactoryTest extends TestCase
+{
+    /**
+     * @var DinosaurFactory
+     */
+    private $factory;
+
+    public function setUp(): void
+    {
+        $this->factory = new DinosaurFactory();
+    }
+
+    public function testItGrowsAVelociraptor()
+    {
+        $dinosaur = $this->factory->growVelociraptor(5);
+
+        $this->assertInstanceOf(Dinosaur::class, $dinosaur);
+        $this->assertIsString($dinosaur->getGenus());
+        $this->assertSame('Velociraptor', $dinosaur->getGenus());
+        $this->assertSame(5, $dinosaur->getLength());
+    }
+
+    public function testItGrowsATriceratops()
+    {
+        $this->markTestIncomplete('Waiting for confirmation from GenLab');
+    }
+
+    public function testItGrowsABabyVelociraptor()
+    {
+        if (!class_exists('Nanny')) {
+            $this->markTestSkipped('There is nobody to watch the baby');
+        }
+
+        $dinosaur = $this->factory->growVelociraptor(1);
+        $this->assertSame(1, $dinosaur->getLength());
+    }
+
+    /**
+     * @dataProvider getSpecificationTests
+     */
+    public function testItGrowsADinasaurFromASpecification(string $spec, bool $expectedIsLarge, bool $expectedIsCarnivorous)
+    {
+        $dinosaur = $this->factory->growFromSpecification($spec);
+
+        if ($expectedIsLarge) {
+            $this->assertGreaterThanOrEqual(Dinosaur::LARGE, $dinosaur->getLength());
+        } else {
+            $this->assertLessThan(Dinosaur::LARGE, $dinosaur->getLength());
+
+        }
+
+        $this->assertSame($expectedIsCarnivorous, $dinosaur->isCarnivorous(), 'Diets do not match');
+    }
+
+    public function getSpecificationTests()
+    {
+        return [
+            // specification, is large, is carnivorous
+            ['large carnivorous dinosaur', true, true],
+            ['give me all the cookiess!!!', false, false],
+            ['large herbivore', true, false]
+        ];
+    }
+}
